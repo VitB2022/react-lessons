a053c1150958f0b7bdfe49be248b2ce8ad241f0c
Vitaliy Bednenko <cupony2018@gmail.com>
2022-08-01 10:11:34 +0300

    added code js and settings for react

---
+++ task3/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  performance: {
+    hints: false,
+  },
+};
