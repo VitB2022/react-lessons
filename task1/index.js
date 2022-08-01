fccfa39d6726accf124356737723f6d1c153fbc3
Vitaliy Bednenko <cupony2018@gmail.com>
2022-08-01 08:24:49 +0300

    added files

---
+++ task1/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const rootElement = document.querySelector('#root');
+const greetingElem = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React!',
+);
+
+ReactDOM.render(greetingElem, rootElement);
