10058156236845d7b8ed60457b07e4f0a90dd67b
Vitaliy Bednenko <cupony2018@gmail.com>
2022-08-01 11:38:22 +0300

    added review build folder

--- task3/webpack.config.js
+++ task3/webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'production',
+  output: {
+    path: __dirname + '/review_build',
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
