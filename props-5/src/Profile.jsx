10faca51aa86c13996ba56d545c3ba66e4f8f615
VitB2022 <105558246+VitB2022@users.noreply.github.com>
2022-08-03 18:46:32 +0300

    added code and settings

---
+++ props-5/src/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import moment from 'moment';
+
+const formatDate = birthDate => moment(birthDate).format('DD MMM YY');
+
+function Profile(props) {
+  return (
+    <div className="profile">
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+      <div className="profile__birth">{`Was born ${formatDate(
+        props.userData.birthDate,
+      )} in ${props.userData.birthPlace}`}</div>
+    </div>
+  );
+}
+
+export default Profile;
