fc91066a28f30c86b5cf5d50ead7bc267296e0cd
VitB2022 <105558246+VitB2022@users.noreply.github.com>
2022-08-05 10:24:50 +0300

    added code

---
+++ events-4/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Color from './Colors.jsx';
+
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Color />, rootElement);
