8932f71abe65ae146079d33fedd84eec342af33d
VitB2022 <105558246+VitB2022@users.noreply.github.com>
2022-08-07 21:27:00 +0300

    added code

---
+++ rendering-4/src/Offline.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Offline = () => (
+  <>
+    <span className="status__text">Offline</span>
+    <button className="status__btn">Reconnect</button>
+  </>
+);
+
+export default Offline;
