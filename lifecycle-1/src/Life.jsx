7349c05a26bb42935b682b7d1aeb403f123164b6
VitB2022 <105558246+VitB2022@users.noreply.github.com>
2022-08-09 12:53:31 +0300

    corrected

--- lifecycle-1/src/Life.jsx
+++ lifecycle-1/src/Life.jsx
@@ -12,7 +12,7 @@ class Life extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log(
-      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render',
+      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
     );
 
     return nextProps.number % 2;
@@ -20,13 +20,13 @@ class Life extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     console.log(
-      'componentDidUpdate(prevProps, prevState): some updates based on new props',
+      'componentDidUpdate(prevProps, prevState): some updates based on new props'
     );
   }
 
   componentWillUnmount() {
     console.log(
-      'componentWillUnmount(): cleanup before DOM related to component will be removed',
+      'componentWillUnmount(): cleanup before DOM related to component will be removed'
     );
   }
   render() {
